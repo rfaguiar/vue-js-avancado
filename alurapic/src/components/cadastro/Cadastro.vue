7ec3ecd8b33480c7707b3334124aa2e18d1ceecf
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-03-06 21:50:48 -0300

    Curso avançado de Vue.js

---
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -0,0 +1,123 @@
+<template>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <h1 v-if="foto._id" class="centralizado">Alteração</h1>
+    <h1 v-else class="centralizado">Inclusão</h1>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input name="titulo" v-model="foto.titulo" id="titulo" autocomplete="off" 
+            v-validate data-vv-rules="required|min:3|max:30" data-vv-as="título" >
+         <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input name="url" v-model="foto.url" id="url" autocomplete="off"
+            v-validate data-vv-rules="required">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link :to="{name: 'home'}"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
+</template>
+
+<script>
+
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../domain/foto/Foto';
+import FotoService from '../../domain/foto/FotoService';
+
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  },
+
+  data () {
+      return {
+          foto: new Foto(),
+          id: this.$route.params.id
+      }
+  }, 
+  methods: {
+
+      grava () {
+
+        this.$validator
+          .validateAll()
+          .then(success => {
+            if(success) {
+
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if(this.id) this.$router.push({ name: 'home'});
+                  this.foto = new Foto()
+                }, 
+                err => console.log(err));
+            }
+        });
+      }
+  }, 
+
+  created() {
+
+    this.service = new FotoService(this.$resource);
+
+    if (this.id) {
+
+        this.service
+            .busca(this.id)
+            .then(foto => this.foto = foto);
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+.erro {
+    color: red;
+  }
+</style>
